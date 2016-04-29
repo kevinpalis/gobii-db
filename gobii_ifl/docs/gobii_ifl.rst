50ac07f2417b431873a571d0dc501adedc9e7e7b
Kevin Palis <KevinPalis@Technicals-MacBook-Pro.local>
2016-04-29 10:37:19 -0400

    Tested out Sphinx

---
+++ gobii_ifl/docs/gobii_ifl.rst
@@ -0,0 +1,29 @@
+gobii_ifl package
+=================
+
+Subpackages
+-----------
+
+.. toctree::
+
+    gobii_ifl.db
+
+Submodules
+----------
+
+gobii_ifl.gobii_ifl module
+--------------------------
+
+.. automodule:: gobii_ifl.gobii_ifl
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: gobii_ifl
+    :members:
+    :undoc-members:
+    :show-inheritance:
--- gobii_ifl/gobii_ifl/db/connection_manager.py
+++ gobii_ifl/gobii_ifl/db/connection_manager.py
@@ -3,6 +3,10 @@ from __future__ import print_function
 import psycopg2
 
 class ConnectionManager:
+	"""
+	This class manages the database connection and initializes connection and cursor objects.
+	You'll have to manually change the connection credentials here.
+	"""
 	conn = None
 	cur = None
 	db_user = "loaderusr"
@@ -15,9 +19,15 @@ class ConnectionManager:
 		print("Database Manager Initialized.")
 
 	def connectToDatabase(self):
+		"""
+		Start a database connection using the credentials set in the class variables.
+		"""
 		self.conn = psycopg2.connect(database=self.db_name, user=self.db_user, password=self.db_pass, host=self.db_host, port=self.db_port)
 		self.cur = self.conn.cursor()
 		return self.conn
 
 	def disconnectFromDatabase(self):
+		"""
+		End a database connection.
+		"""
 		self.conn.close()
