f778f0b0972be20bedf4b80900f11a1c297c73a8
Kevin Palis <KevinPalis@Technicals-MacBook-Pro.local>
2016-05-20 15:30:43 -0400

    GP1-383 Added the integrity check as an optional feature, set by the -l flag

--- gobii_ifl/gobii_ifl/gobii_ifl.py
+++ gobii_ifl/gobii_ifl/gobii_ifl.py
@@ -10,13 +10,14 @@ from util.ifl_utility import IFLUtility
 
 def main(argv):
 		verbose = False
+		flCheck = False
 		connectionStr = ""
 		iFile = ""
 		inputDir = ""
 		outputPath = ""
 		#print("Args count: ", len(argv))
 		try:
-			opts, args = getopt.getopt(argv, "hc:i:d:o:v", ["connectionString=", "inputFile=", "inputDir=", "outputDir=", "verbose"])
+			opts, args = getopt.getopt(argv, "hc:i:d:o:vl", ["connectionString=", "inputFile=", "inputDir=", "outputDir=", "verbose", "fileLengthCheck"])
 			#print (opts, args)
 			if len(args) < 3 and len(opts) < 3:
 				printUsageHelp()
@@ -36,6 +37,8 @@ def main(argv):
 				outputPath = arg
 			elif opt in ("-v", "--verbose"):
 				verbose = True
+			elif opt in ("-l", "--fileLengthCheck"):
+				flCheck = True
 		#if verbose:
 		#print("Opts: ", opts)
 		if connectionStr != "" and outputPath != "":
@@ -45,7 +48,10 @@ def main(argv):
 						if verbose:
 							print("Processing file %s..." % f)
 						preprocessedFile = preprocess_ifile.main(verbose, connectionStr, os.path.join(inputDir, f), outputPath)
-						loadFile = load_ifile.main(verbose, connectionStr, preprocessedFile, outputPath)
+						if flCheck and not checkDataIntegrity(f, preprocessedFile, verbose):
+							IFLUtility.printError("File length mismatch detected on %s. You have duplicate entries in the table where the NMAP file maps to, please fix it. Skipping file." % f)
+						else:
+							loadFile = load_ifile.main(verbose, connectionStr, preprocessedFile, outputPath)
 						try:
 							os.remove(preprocessedFile)
 							os.remove(loadFile)
@@ -55,7 +61,11 @@ def main(argv):
 						IFLUtility.printError("Failed to load file %s. Error: %s" % (f, str(e1)))
 			elif iFile != "":
 				preprocessedFile = preprocess_ifile.main(verbose, connectionStr, iFile, outputPath)
-				loadFile = load_ifile.main(verbose, connectionStr, preprocessedFile, outputPath)
+				loadFile = None
+				if flCheck and not checkDataIntegrity(iFile, preprocessedFile, verbose):
+					IFLUtility.printError("File length mismatch detected on %s. You have duplicate entries in the table where the NMAP file maps to, please fix it first. Loading will abort." % iFile)
+				else:
+					loadFile = load_ifile.main(verbose, connectionStr, preprocessedFile, outputPath)
 				try:
 					os.remove(preprocessedFile)
 					os.remove(loadFile)
@@ -67,6 +77,18 @@ def main(argv):
 			printUsageHelp()
 		#cleanup
 
+def checkDataIntegrity(iFile, pFile, verbose):
+	iCount = IFLUtility.getFileLineCount(iFile)
+	pCount = IFLUtility.getFileLineCount(pFile)
+	#print ("Input file line count: %i" % iCount)
+	#print ("Ppd file line count: %i" % pCount)
+	if iCount == pCount:
+		return True
+	else:
+		if verbose:
+			print ("Mismatch: input_file=%s preprocessed_file=%s" % (iCount, pCount))
+		return False
+
 def printUsageHelp():
 	print ("gobii_ifl.py -c <connectionString> -i <inputFile> -d <inputDir> -o <outputDirectory> -v")
 	print ("\t-h = Usage help")
@@ -75,6 +97,7 @@ def printUsageHelp():
 	print ("\t-d or --inputDir = The input directory. The IFL will load each file found in this directory. \n\t\tIn case both inputFile and inputDir is specified, inputDir will take precedence.")
 	print ("\t-o or --outputDir = The output directory where preprocessed file and file for bulk loading (no duplicates) will be placed.\n\t\tEnsure that this path is writeable.")
 	print ("\t-v or --verbose = Print the status of the IFL in more detail")
+	print ("\t-l or --fileLengthCheck = This will check if the preprocessed file is of the same length as the input file. \n\t\tA mismatch indicates duplicate entries in the table where the NMAP file maps to.\n\t\tInput file should not be loaded in that case.")
 	sys.exit()
 
 if __name__ == "__main__":
--- gobii_ifl/gobii_ifl/util/ifl_utility.py
+++ gobii_ifl/gobii_ifl/util/ifl_utility.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import string
 import random
 import sys
+import subprocess
 
 class IFLUtility:
 	"""
@@ -21,3 +22,11 @@ class IFLUtility:
 	@staticmethod
 	def printError(*args, **kwargs):
 		print(*args, file=sys.stderr, **kwargs)
+
+	@staticmethod
+	def getFileLineCount(fname):
+		p = subprocess.Popen(['wc', '-l', fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		result, err = p.communicate()
+		if p.returncode != 0:
+			raise IOError(err)
+		return int(result.strip().split()[0])
