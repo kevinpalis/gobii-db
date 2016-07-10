c13147a053d0832f906568ee5f2870b357cecd6c
Kevin Palis <kevin.palis@gmail.com>
2016-07-10 11:53:27 +0800

    fixed the join issues on nullable ever changing fields

--- builder/functions.sql
+++ builder/functions.sql
@@ -1727,6 +1727,66 @@ $$ LANGUAGE plpgsql;
 -- Extraction functions 
 --######################################################################
 --drop function getAllMarkerMetadataByDataset(datasetId integer);
+CREATE OR REPLACE FUNCTION getMinimalMarkerMetadataByDataset(datasetId integer)
+RETURNS table (marker_name text, alleles text, chrom varchar, pos integer, strand text) AS $$
+  BEGIN
+    return query
+    with dm as (select dm.marker_id, dm.marker_idx from dataset_marker dm where dm.dataset_id=datasetId)
+    select m.name as marker_name, m.ref || '/' || array_to_string(m.alts, ',', '?') as alleles, mlp.linkage_group_name as chrom, mlp.stop as pos, cv.term as strand
+    from dm inner join marker m on m.marker_id = dm.marker_id 
+    left join v_marker_linkage_genetic mlp on m.marker_id = mlp.marker_id
+    left join cv on m.strand_id = cv.cv_id
+    order by dm.marker_idx;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION getAllMarkerMetadataByDataset(datasetId integer)
+RETURNS table (marker_name text, linkage_group_name varchar, start numeric, stop numeric, mapset_name text, platform_name text, variant_id integer, code text, ref text, alts text, sequence text, reference_name text, primers jsonb, probsets jsonb, strand_name text) AS $$
+  BEGIN
+    return query
+    with dm as (select dm.marker_id, dm.marker_idx from dataset_marker dm where dm.dataset_id=datasetId)
+    select m.name as marker_name, mlp.linkage_group_name, mlp.start, mlp.stop, mlp.mapset_name, p.name as platform_name, m.variant_id, m.code, m.ref, array_to_string(m.alts, ',', '?'), m.sequence, r.name as reference_name, m.primers, m.probsets, cv.term as strand_name
+      from marker m inner join platform p on m.platform_id = p.platform_id
+      inner join dm on m.marker_id = dm.marker_id 
+      left join reference r on m.reference_id = r.reference_id
+      left join cv on m.strand_id = cv.cv_id 
+      left join v_marker_linkage_physical mlp on m.marker_id = mlp.marker_id
+      order by dm.marker_idx;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION getMinimalSampleMetadataByDataset(datasetId integer)
+RETURNS table (dnarun_name text, sample_name text, germplasm_name text, external_code text, germplasm_type text, species text, platename text, num text, well_row text, well_col text) AS $$
+  BEGIN
+    return query
+    with dd as (select dd.dnarun_id, dd.dnarun_idx from dataset_dnarun dd where dd.dataset_id=datasetId)
+    select dr.name as dnarun_name, ds.name as sample_name, g.name as germplasm_name, g.external_code, c1.term as germplasm_type, c2.term as species, ds.platename, ds.num, ds.well_row, ds.well_col
+    from dd inner join dnarun dr on dr.dnarun_id = dd.dnarun_id 
+    inner join dnasample ds on dr.dnasample_id = ds.dnasample_id 
+    inner join germplasm g on ds.germplasm_id = g.germplasm_id 
+    left join cv as c1 on g.type_id = c1.cv_id 
+    left join cv as c2 on g.species_id = c2.cv_id
+    order by dd.dnarun_idx;
+  END;
+$$ LANGUAGE plpgsql;
+ 
+CREATE OR REPLACE FUNCTION getAllSampleMetadataByDataset(datasetId integer)
+RETURNS table (dnarun_name text, sample_name text, germplasm_name text, external_code text, germplasm_type text, species text, platename text, num text, well_row text, well_col text) AS $$
+  BEGIN
+    return query
+    with dd as (select dd.dnarun_id, dd.dnarun_idx from dataset_dnarun dd where dd.dataset_id=datasetId)
+      select dr.name as dnarun_name, ds.name as sample_name, g.name as germplasm_name, g.external_code, c1.term as germplasm_type, c2.term as species, ds.platename, ds.num, ds.well_row, ds.well_col
+      from dd inner join dnarun dr on dr.dnarun_id = dd.dnarun_id 
+      inner join dnasample ds on dr.dnasample_id = ds.dnasample_id 
+      inner join germplasm g on ds.germplasm_id = g.germplasm_id 
+      left join cv as c1 on g.type_id = c1.cv_id 
+      left join cv as c2 on g.species_id = c2.cv_id
+      order by dd.dnarun_idx;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+/*
 CREATE OR REPLACE FUNCTION getAllMarkerMetadataByDataset(datasetId integer)
 RETURNS table (marker_name text, linkage_group_name varchar, start numeric, stop numeric, mapset_name text, platform_name text, variant_id integer, code text, ref text, alts text, sequence text, reference_name text, primers jsonb, probsets jsonb, strand_name text) AS $$
   BEGIN
@@ -1757,6 +1817,7 @@ RETURNS table (marker_name text, alleles text, chrom varchar, pos integer, stran
       order by dm.marker_idx;
   END;
 $$ LANGUAGE plpgsql;
+*/
 
 CREATE OR REPLACE FUNCTION getMarkerNamesByDataset(datasetId integer)
 RETURNS table (marker_id integer, marker_name text) AS $$
@@ -1770,6 +1831,7 @@ RETURNS table (marker_id integer, marker_name text) AS $$
   END;
 $$ LANGUAGE plpgsql;
 
+/*
 --drop function getAllSampleMetadataByDataset(datasetId integer);
 CREATE OR REPLACE FUNCTION getAllSampleMetadataByDataset(datasetId integer)
 RETURNS table (dnarun_name text, dnasample_name text, platename text, num text, well_row text, well_col text, germplasm_name text, external_code text, germplasm_type text, species text) AS $$
@@ -1803,7 +1865,7 @@ RETURNS table (sample_name text, platename text, num text, well_row text, well_c
       order by dd.dnarun_idx;
   END;
 $$ LANGUAGE plpgsql;
-
+*/
 CREATE OR REPLACE FUNCTION getDnarunNamesByDataset(datasetId integer)
 RETURNS table (dnarun_id integer, dnarun_name text) AS $$
   BEGIN
