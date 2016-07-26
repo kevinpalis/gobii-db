16c83099bb963d7374352a15544564ff8d2dbeef
Kevin Palis <kevin.palis@gmail.com>
2016-07-26 23:43:55 +0800

    Changed problematic pw

--- builder/create_dbuser.sh
+++ builder/create_dbuser.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-psql -U postgres -c "create user appuser with superuser password 'g0b11is@w3s0m3' valid until 'infinity';"
+psql -U postgres -c "create user appuser with superuser password 'g0b11isw3s0m3' valid until 'infinity';"
