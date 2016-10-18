0d6dee205a6665f25fda04b874b1bf9187338bc2
Kevin Palis <kevin.palis@gmail.com>
2016-10-18 15:46:23 -0400

    Testing auto mkdir in sys_int - take 2

--- builder/rawbase/create_or_replace_gobii_db.sh
+++ builder/rawbase/create_or_replace_gobii_db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #params: $1 = database_name; $2 = user_name
-#change to test
+
 psql -h $3 -p $4 -U $2 postgres -c "drop database $1;"
 psql -h $3 -p $4 -U $2 postgres -c "create database $1 owner $2;"
 psql -h $3 -p $4 -U $2 $1 -f build_gobii_pg.sql
