7bac97adc02304bed6c55b3685133a91768bd7cd
Kevin Palis <kevin.palis@gmail.com>
2016-10-18 16:04:02 -0400

    Trigger test

--- builder/rawbase/create_or_replace_gobii_db.sh
+++ builder/rawbase/create_or_replace_gobii_db.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #params: $1 = database_name; $2 = user_name
+#trigger
 export PGPASSWORD="$6"
 psql -h $3 -p $4 -U $2 postgres -c "drop database $1;"
 psql -h $3 -p $4 -U $2 postgres -c "create database $1 owner $2;"
