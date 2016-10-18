0acd6cf96b9c1f7bbaf8460330328e88ce008df1
Kevin Palis <kevin.palis@gmail.com>
2016-10-18 19:31:57 -0400

    Took a while, but this now should work. Used PGPASS to bypass interactive mode psql

--- builder/rawbase/create_or_replace_gobii_db.sh
+++ builder/rawbase/create_or_replace_gobii_db.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #params: $1 = database_name; $2 = user_name
 #trigger
-export PGPASSWORD="$6"
-psql -h $3 -p $4 -U $2 postgres -c "drop database $1;"
-psql -h $3 -p $4 -U $2 postgres -c "create database $1 owner $2;"
-psql -h $3 -p $4 -U $2 $1 -f build_gobii_pg.sql
+echo $1 $2 $3 $4 $5
+export PGPASSWORD=$5
+psql -h "$3" -p $4 -U $2 postgres -c "drop database $1;"
+psql -h "$3" -p $4 -U $2 postgres -c "create database $1 owner $2;"
+psql -h "$3" -p $4 -U $2 $1 -f build_gobii_pg.sql
