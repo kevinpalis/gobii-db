b100987cfbcd1546a3fee60279b7dbf6f76f4867
Kevin Palis <kevin.palis@gmail.com>
2016-10-18 19:40:48 -0400

    Added printing to test trigger

--- builder/rawbase/create_or_replace_gobii_db.sh
+++ builder/rawbase/create_or_replace_gobii_db.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #params: $1 = database_name; $2 = user_name
 #trigger
-echo $1 $2 $3 $4 $5
+echo "Params: " $1 $2 $3 $4 $5
 export PGPASSWORD=$5
 echo "Dropping database $1..."
 psql -h "$3" -p $4 -U $2 postgres -c "drop database $1;"
