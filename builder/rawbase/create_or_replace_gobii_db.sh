11fc042b3f3b32a1d627e46c6468cee8b15f16b7
Kevin Palis <kevin.palis@gmail.com>
2016-10-18 15:04:58 -0400

    Again, just to test the trigger, as the first one didn't work.

--- builder/rawbase/create_or_replace_gobii_db.sh
+++ builder/rawbase/create_or_replace_gobii_db.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #params: $1 = database_name; $2 = user_name
 psql -U $2 postgres -c "drop database $1;"
+psql -U $2 postgres -c "drop database $1;"
 psql -U $2 postgres -c "create database $1 owner $2;"
 psql -U $2 $1 -f build_gobii_pg.sql
