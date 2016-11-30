dda9876dd69795d123ba96a6166ee648d47a3346
Raza <smr337@cornell.edu>
2016-11-30 16:22:37 -0500

    modified psql function to repeat rows for  markers but display only given map info

--- builder/liquibase/changelogs/changesets/mde_mapset_info.sql
+++ builder/liquibase/changelogs/changesets/mde_mapset_info.sql
@@ -4,22 +4,36 @@
 DROP FUNCTION IF EXISTS getMarkerMapsetInfoByDataset( integer, integer);
 -- This
 CREATE OR REPLACE FUNCTION getMarkerMapsetInfoByDataset(dsId integer,mapId integer)
-RETURNS table (marker_name text,platform text,linkage_group_name text,linkage_group_start integer,linkage_group_stop integer,marker_linkage_group_start integer,marker_linkage_group_stop integer,mapset_name text) AS $$
+RETURNS table (marker_name text,platform text,linkage_group_name text,linkage_group_start text,linkage_group_stop text,marker_linkage_group_start text,marker_linkage_group_stop text,mapset_name text) AS $$
 BEGIN
 	RETURN QUERY 
 	with mlgt as (
-		select mlg.marker_id, lg.name as linkage_group_name, lg.start as lg_start,lg.start as lg_stop,  mlg.start, mlg.stop, ms.name as mapset_name
-		from marker_linkage_group mlg
-		left join linkage_group lg on lg.linkage_group_id = mlg.linkage_group_id
-		left join mapset ms on ms.mapset_id = lg.map_id
-		where lg.map_id = mapId
-	)
-	select m.name,p.name,mlgt.linkage_group_name::text, mlgt.lg_start, mlgt.lg_stop,mlgt.start::integer,mlgt.stop::integer,mlgt.mapset_name
-	from marker m
-	join platform p on p.platform_id = m.platform_id
-	left join mlgt on mlgt.marker_id = m.marker_id
-	where m.dataset_marker_idx ? dsId::text
-	order by m.dataset_marker_idx -> dsId::text;
+			select distinct on (mlg.marker_id, mapset_id) mlg.marker_id, lg.name as linkage_group_name, lg.start as lg_start,lg.start as lg_stop,  mlg.start, mlg.stop,ms.mapset_id, ms.name as mapset_name
+			from marker_linkage_group mlg
+			left join linkage_group lg on lg.linkage_group_id = mlg.linkage_group_id
+			left join mapset ms on ms.mapset_id = lg.map_id
+			join marker mr on mr.marker_id = mlg.marker_id and mr.dataset_marker_idx ? dsId::text		
+		)
+		select m.name,p.name
+			,COALESCE(t.lgn,mlgt.linkage_group_name::text) as lg_name 
+			,COALESCE(t.lgst,mlgt.lg_start::text) as lg_start
+			,COALESCE(t.lgsp,mlgt.lg_stop::text) as lg_stop
+			,COALESCE(t.mlgst,mlgt.start::text) as mlg_start
+			,COALESCE(t.mlgst,mlgt.stop::text) as mlg_stop
+			, COALESCE(t.mpsn,mlgt.mapset_name) as mapset_name
+		from marker m
+		join platform p on p.platform_id = m.platform_id
+		left join mlgt on mlgt.marker_id = m.marker_id 
+		left join (	
+			select  ''::text as lgn
+				,''::text as mpsn	
+				,''::text as lgst
+				,''::text as lgsp
+				,''::text as mlgst
+				,''::text as mlgsp			
+		) t on mlgt.mapset_id != mapId
+		where m.dataset_marker_idx ? dsId::text
+		order by m.dataset_marker_idx -> dsId::text;
 END;
 $$ LANGUAGE plpgsql;
 
