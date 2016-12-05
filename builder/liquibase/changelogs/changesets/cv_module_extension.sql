cc52f4f898842d16f1f7f69aae0416d483290ffa
Kevin Palis <kevin.palis@gmail.com>
2016-12-05 01:20:58 -0500

    Almost done, but not quite. Migration's tricky.

--- builder/liquibase/changelogs/changesets/cv_module_extension.sql
+++ builder/liquibase/changelogs/changesets/cv_module_extension.sql
@@ -7,13 +7,13 @@ CREATE TABLE cvgroup (
   cvgroup_id           serial  NOT NULL,
   name                 text  NOT NULL,
   definition           text  ,
-  type_id              integer  ,
+  type              integer  NOT NULL DEFAULT 1,
   props                jsonb DEFAULT '{}'::jsonb,
   CONSTRAINT cv_pkey PRIMARY KEY ( cvgroup_id ),
   CONSTRAINT unique_cvgroup_name UNIQUE ( name ) 
  );
 
-CREATE INDEX idx_cvgroup ON cvgroup ( type_id );
+CREATE INDEX idx_cvgroup ON cvgroup ( type );
 
 COMMENT ON TABLE cvgroup IS 'A controlled vocabulary or ontology. A cv is
 composed of cvterms (AKA terms, classes, types, universals - relations
@@ -22,22 +22,33 @@ between them.';
 
 COMMENT ON COLUMN cvgroup.name IS 'The name of the group.';
 
-COMMENT ON COLUMN cvgroup.definition IS 'A text description of the criteria for
-membership of this group.';
+COMMENT ON COLUMN cvgroup.definition IS 'A text description of the criteria for membership of this group.';
 
-COMMENT ON COLUMN cvgroup.type_id IS 'Determines if CV group is of type "System CV" or "Custom CV". This is extensible.';
-
-ALTER TABLE cvgroup ADD CONSTRAINT cvgroup_typeid_fkey FOREIGN KEY ( type_id ) REFERENCES cv( cv_id );
+COMMENT ON COLUMN cvgroup.type IS 'Determines if CV group is of type "System CV" (1) or "Custom CV" (2). More types can be added as needed';
 
 --## CV ##--
---changeset kpalis:normalize_cv_group context:general splitStatements:false
+--changeset kpalis:normalize_and_populate_cv_group context:general splitStatements:false
 
-ALTER TABLE cv ADD COLUMN cvgroup_id integer NOT NULL default 1;
+ALTER TABLE cv ADD COLUMN cvgroup_id integer NOT NULL;
 
 ALTER TABLE cv ADD CONSTRAINT unique_cvterm_term_cvgroupid UNIQUE ( term, cvgroup_id );
 ALTER TABLE cv ADD CONSTRAINT cv_cvgroupid_fkey FOREIGN KEY ( cvgroup_id ) REFERENCES cvgroup( cvgroup_id );
 --migrate data: group name -> group id
+INSERT INTO cvgroup (name, definition, type) values ('dataset_type', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('germplasm_prop', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('dnarun_prop', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('status', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('marker_strand', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('germplasm_species', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('marker_prop', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('dnasample_prop', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('platform_type', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('analysis_type', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('project_prop', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('germplasm_type', '', 1);
+INSERT INTO cvgroup (name, definition, type) values ('mapset_type', '', 1);
 
+-----
 
 CREATE INDEX idx_cv_cvgroupid ON cv ( cvgroup_id );
 --drop freetext group column
