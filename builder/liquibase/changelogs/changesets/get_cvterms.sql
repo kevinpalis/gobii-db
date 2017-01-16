3e3e4e46878cdee6e7ff69f9413a74e5496f9eb5
Venice Margarette Juanillas <vjuanillas@gobiidb.hpc.irri.org>
2017-01-16 09:36:38 +0800

    Fixed bug: missing RETURN query

--- builder/liquibase/changelogs/changesets/get_cvterms.sql
+++ builder/liquibase/changelogs/changesets/get_cvterms.sql
@@ -6,8 +6,8 @@ DROP FUNCTION IF EXISTS getCvtermsByCvgroupname(cvgroupName text);
 CREATE OR REPLACE FUNCTION getCvtermsByCvgroupname(cvgroupName text)
 RETURNS TABLE (term text) AS $$
 BEGIN
+	RETURN query
 	select cv.term from cv, cvgroup
 	where cv.cvgroup_id = cvgroup.cvgroup_id and cvgroup.name = cvgroupName;
-	
 END;
 $$ LANGUAGE plpgsql;
