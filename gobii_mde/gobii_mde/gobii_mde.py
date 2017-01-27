6e1c211ae64455a6907ef57305c3a0e71798155f
Kevin Palis <kevin.palis@gmail.com>
2017-01-27 17:21:15 -0500

    Adding a parameter to be able to set mapsetOutputFilename

--- gobii_mde/gobii_mde/gobii_mde.py
+++ gobii_mde/gobii_mde/gobii_mde.py
@@ -29,10 +29,11 @@ def main(argv):
 		displayMap = -1
 		markerListFile = ""
 		sampleListFile = ""
+		mapsetOutputFile = ""
 		exitCode = 0
 		#print("Args count: ", len(argv))
 		try:
-			opts, args = getopt.getopt(argv, "hc:m:s:d:p:avnM:lD:x:y:", ["connectionString=", "markerOutputFile=", "sampleOutputFile=", "datasetId=", "projectOutputFile=", "all", "verbose", "namesOnly", "map=", "includeChrLen", "displayMap=", "markerList=", "sampleList="])
+			opts, args = getopt.getopt(argv, "hc:m:s:d:p:avnM:lD:x:y:b", ["connectionString=", "markerOutputFile=", "sampleOutputFile=", "datasetId=", "projectOutputFile=", "all", "verbose", "namesOnly", "map=", "includeChrLen", "displayMap=", "markerList=", "sampleList=", "mapsetOutputFile="])
 			#print (opts, args)
 			if len(args) < 2 and len(opts) < 2:
 				printUsageHelp()
@@ -68,6 +69,8 @@ def main(argv):
 				markerListFile = arg
 			elif opt in ("-y", "--sampleList"):
 				sampleListFile = arg
+			elif opt in ("-b", "--mapsetOutputFile"):
+				mapsetOutputFile = arg
 
 		#if verbose:
 		#print("Opts: ", opts)
@@ -126,7 +129,7 @@ def main(argv):
 		#cleanup
 
 def printUsageHelp():
-	print ("gobii_mde.py -c <connectionString> -m <markerOutputFile> -s <sampleOutputFile> -p <projectOutputFile> -d <dataset_id> -M <map_id> -D <MapsetId for Display> -a -v -n")
+	print ("gobii_mde.py -c <connectionString> -m <markerOutputFile> -s <sampleOutputFile> -p <projectOutputFile> -d <dataset_id> -M <mapset_id> -D <mapset_id> for Display> -a -v -n")
 	print ("\t-h = Usage help")
 	print ("\t-c or --connectionString = Database connection string (RFC 3986 URI).\n\t\tFormat: postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]")
 	print ("\t-m or --markerOutputFile = The marker metadata output file. This should be an absolute path.")
@@ -136,9 +139,9 @@ def printUsageHelp():
 	print ("\t-a or --all = [OBSOLETE] Currently the 'all' metadata is actually less columns than the default. So don't use this for now. We may offer a more verbose output than the default in the future.")
 	print ("\t-v or --verbose = Print the status of the MDE in more detail.")
 	print ("\t-n or --namesOnly = [OBSOLETE] This was originally requested to link postgres and monet. But this is not being used and the business logic of the MDEs changed significantly, leaving this behind.")
-	print ("\t-M or --map = Get only the markers in the specified map. This is useful if a dataset contains markers that belongs to multiple maps.")
+	print ("\t-M or --map = This filters the result by mapset, ie. get only the markers in the specified mapset. This is useful if a dataset contains markers that belongs to multiple maps.")
 	print ("\t-l or --includeChrLen = Generates a file that lists all the chromosomes (or any linkage groups) that appear on the markers list, along with their lengths. Filename is the same as the marker file but appended with .chr.")
-	print ("\t-D or --displayMap = MapsetId for the mapset info to display, if marker exists in more than one mapset. Mapset file is the same as marker file appended  with .mapset.")
+	print ("\t-D or --displayMap = This creates two files: one with the marker metadata appended with the map data of the selected mapset (marker_filename.ext) and a mapset metadata file. The mapset metadata filename is fetched from the value passed to  the -b (--mapsetOutputFile) parameter, if the -b parameter is not set, it defaults to marker_filename.mapset.")
 	print ("\t-x or --markerList = Supplies the file containing a list of marker_ids, newline-delimited. Setting this instructs the MDE to do extraction by marker list.")
 	print ("\t-y or --sampleList = Supplies the file containing a list of dnarun_ids, newline-delimited. Setting this instructs the MDE to do extraction by dnarun list.")
 	sys.exit(1)
