91b581f0d17c087969da588511465b6e1a7bb785
Kevin Palis <kevin.palis@gmail.com>
2017-02-07 12:57:00 -0500

    Commiting to test if Bamboo can pick up the test results as JUnit XMLs

---
+++ test/loading/gobii_loading_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import unittest
+import xmlrunner
+
+class GLoadingTest(unittest.TestCase):
+	def test(self):
+		a = 'a'
+		b = 'a'
+		self.assertEqual(a, b)
+
+	def test_upper(self):
+		self.assertEqual('foo'.upper(), 'FOO')
+
+	def test_isupper(self):
+		self.assertTrue('FOO'.isupper())
+		self.assertFalse('Foo'.isupper())
+
+	def test_split(self):
+		s = 'hello world'
+		self.assertEqual(s.split(), ['hello', 'world'])
+		# check that s.split fails when the separator is not a string
+		with self.assertRaises(TypeError):
+			s.split(2)
+
+
+if __name__ == '__main__':
+	#unittest.main()
+	unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
