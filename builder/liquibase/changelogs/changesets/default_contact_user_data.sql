63eb4eebad725a8aa749953dc847b4f78c490527
raza <smr337@cornell.edu>
2017-04-21 16:07:42 -0400

    updating username.

--- builder/liquibase/changelogs/changesets/default_contact_user_data.sql
+++ builder/liquibase/changelogs/changesets/default_contact_user_data.sql
@@ -2,4 +2,4 @@
 
 --changeset raza:default_contact_user_data context:seed_general splitStatements:false    
 select * from createcontact('GOBII','User1','default_user_1','user1.gobii@gobii.org','{5,1,6}',1,current_date,1,current_date,NULL,'USER_READER');
-select * from createcontact('GOBII Ldap','User2','default_user_1','user2.gobii@gobii.org','{5,1,6}',1,current_date,1,current_date,NULL,'gobii-user1');
+select * from createcontact('GOBII Ldap','User2','default_user_2','user2.gobii@gobii.org','{5,1,6}',1,current_date,1,current_date,NULL,'gobii-user2');
