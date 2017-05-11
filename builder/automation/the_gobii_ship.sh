4615a7f32c67a42fbe26b8c807eb90560f763d6b
Kevin Palis <kevin.palis@gmail.com>
2017-05-11 04:18:02 -0400

    last last LAST test

--- builder/automation/the_gobii_ship.sh
+++ builder/automation/the_gobii_ship.sh
@@ -44,9 +44,12 @@ docker start $DOCKER_DB_NAME;
 
 #set the proper UID and GID and chown the hell out of everything (within the docker, of course)
 echo "Matching the docker gadm account to that of the host and changing file ownerships..."
-DOCKER_CMD="usermod -u $GOBII_UID gadm; groupmod -g $GOBII_GID gobii; find / -user 1000 -exec chown -h $GOBII_UID {} \; find / -group 1001 -exec chgrp -h $GOBII_GID {} \;"
+DOCKER_CMD="'usermod -u $GOBII_UID gadm; 
+groupmod -g $GOBII_GID gobii; 
+find / -user 1000 -exec chown -h $GOBII_UID {} \; 
+find / -group 1001 -exec chgrp -h $GOBII_GID {} \;'"
 #echo "DOCKER_CMD: " $DOCKER_CMD
-eval "docker exec $DOCKER_DB_NAME bash -c ' $DOCKER_CMD '";
+eval "docker exec $DOCKER_DB_NAME bash -c $DOCKER_CMD";
 
 #clear the target directory of any old gobii_bundle
 echo "Copying the GOBII_BUNDLE to the shared directory/volume..."
