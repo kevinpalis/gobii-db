1b207e925bb049d71a255b94c4d2592af3319e89
Kevin Palis <kevin.palis@gmail.com>
2017-11-30 11:56:49 -0500

    Minor changes on table elements names

--- builder/liquibase/changelogs/changesets/create_timescoper_module.sql
+++ builder/liquibase/changelogs/changesets/create_timescoper_module.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --### Administrator Module for the GOBII Data TimeScope Web Application ###---
-
+-- Roles: 1=Super Admin, 2=Admin, 3=User (reserved for foreseen functionality requests)
 --changeset kpalis:create_administrator_table context:general splitStatements:false
 
 CREATE TABLE IF NOT EXISTS public.timescoper
@@ -10,9 +10,9 @@ CREATE TABLE IF NOT EXISTS public.timescoper
     firstname text NOT NULL,
     lastname text NOT NULL,
     username text NOT NULL,
-    password text NOT NULL
+    password text NOT NULL,
     email text,
     role integer default 3,
-    CONSTRAINT pk_contact PRIMARY KEY (timescoper_id),
+    CONSTRAINT pk_timescoper PRIMARY KEY (timescoper_id),
     CONSTRAINT username_key UNIQUE (username)
 );
