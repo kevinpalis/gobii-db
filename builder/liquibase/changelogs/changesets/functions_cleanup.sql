88f7591629c805e586ff32c7a73b1f9ea39da2ba
Kevin Palis <kevin.palis@gmail.com>
2018-01-08 13:50:22 -0500

    Now checked more than half of the functions.

--- builder/liquibase/changelogs/changesets/functions_cleanup.sql
+++ builder/liquibase/changelogs/changesets/functions_cleanup.sql
@@ -5,4 +5,7 @@ DROP FUNCTION IF EXISTS addanalysistodataset(integer,integer);
 DROP FUNCTION IF EXISTS createdatasetmarker(integer, integer, real, real, real, jsonb);
 DROP FUNCTION IF EXISTS createmarkerlinkagegroup(integer, integer, integer, integer);
 
---@deleteanalysis
+DROP FUNCTION IF EXISTS getsampleqcmetadatabymarkerlistx(text, text);
+--!! DROP getcontactnamesbyrole AND getcontactsbyrole IF THEY AREN'T USED. They use refcursors which is not the preferred way. This is probably a remnant of the old code.
+
+--now at updateanalysis() -- halfway done!
