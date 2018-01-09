0972c2cfaf2f77cbf8d19075b5497fa5c7426d89
Kevin Palis <kevin.palis@gmail.com>
2018-01-09 12:48:47 -0500

    Unfortunately, these are the only ones I can drop as of now. We still have more than 200 functions

--- builder/liquibase/changelogs/changesets/functions_cleanup.sql
+++ builder/liquibase/changelogs/changesets/functions_cleanup.sql
@@ -1,11 +1,14 @@
 --liquibase formatted sql
 
---### Functions cleanup. Deleting functions that are broken -- which means they were never used. ###---
+--### Functions cleanup. Deleting functions that are broken or redundant. ###---
+--changeset kpalis:drop_broken_functions context:general splitStatements:false
 DROP FUNCTION IF EXISTS addanalysistodataset(integer,integer);
 DROP FUNCTION IF EXISTS createdatasetmarker(integer, integer, real, real, real, jsonb);
 DROP FUNCTION IF EXISTS createmarkerlinkagegroup(integer, integer, integer, integer);
 
 DROP FUNCTION IF EXISTS getsampleqcmetadatabymarkerlistx(text, text);
---!! DROP getcontactnamesbyrole AND getcontactsbyrole IF THEY AREN'T USED. They use refcursors which is not the preferred way. This is probably a remnant of the old code.
 
---now at updateanalysis() -- halfway done!
+DROP FUNCTION IF EXISTS getcontactnamesbyrole(character varying);
+DROP FUNCTION IF EXISTS getcontactsbyrole(character varying);
+
+DROP FUNCTION IF EXISTS updatemarkerlinkagegroup(integer, integer, integer, integer, integer);
