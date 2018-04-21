80a54b3ce4a0529da337a81949ce8cd137c3f489
Kevin Palis <kevin.palis@gmail.com>
2018-04-21 11:12:52 -0400

    Missed the id column for filtering

--- builder/liquibase/changelogs/changesets/run_on_change/utility_views.sql
+++ builder/liquibase/changelogs/changesets/run_on_change/utility_views.sql
@@ -4,9 +4,9 @@
 
 --this view is intended to always be used with a where clause
 --changeset kpalis:view_for_markergroup_summary context:general splitStatements:false runOnChange:true
-create view v_marker_group_summary as
+create or replace view v_marker_group_summary as
 	select mg.marker_group_id, mg.name as marker_group_name, mg.germplasm_group, m.name as marker_name, p.name as platform, array_to_string(ARRAY(SELECT jsonb_array_elements_text(mg.value)), ',', '?') as favorable_alleles
-	from marker m, platform p, (select name, germplasm_group, (jsonb_each(markers)).* from marker_group) as mg
+	from marker m, platform p, (select marker_group_id, name, germplasm_group, (jsonb_each(markers)).* from marker_group) as mg
 	where m.marker_id = mg.key::integer
 	and p.platform_id = m.platform_id;
 
