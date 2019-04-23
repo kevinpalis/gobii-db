85f34952d1d2f18732363378eacda275d0137209
Kevin Palis <kevin.palis@gmail.com>
2019-04-23 13:54:56 -0400

    Adding the changeset for uuid column

--- builder/liquibase/changelogs/changesets/2.1/add_sample_uuid.sql
+++ builder/liquibase/changelogs/changesets/2.1/add_sample_uuid.sql
@@ -3,4 +3,15 @@
 --Adding column uuid to dnasample
 
 
---changeset kpalis:add_sample_uuid context:general splitStatements:false runOnChange:true
+--changeset kpalis:add_sample_uuid_col context:general splitStatements:false runOnChange:false
+ALTER TABLE dnasample ADD COLUMN uuid text;
+--update the existing data so we can add a not null constraint
+UPDATE dnasample
+  SET uuid = 'Pre-2.1_uuid_' || dnasample_id::text
+  WHERE uuid IS NULL;
+--add the not null and uniqueness constraints
+ALTER TABLE dnasample ALTER COLUMN uuid SET NOT NULL;
+ALTER TABLE dnasample ADD CONSTRAINT unique_dnasample_uuid UNIQUE ( uuid );
+
+--NOTE: Functions will be added on a separate file that's already on a runOnChange=true.
+-- This is an effort to consolidate and organize the many CRUD functions
