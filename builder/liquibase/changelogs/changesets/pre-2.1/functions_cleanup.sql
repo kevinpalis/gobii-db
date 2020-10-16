f877ac364bba6caeae698f5a772f081cc669fcc2
Kevin Palis <kevin.palis@gmail.com>
2020-10-16 15:13:16 -0400

    This removes the clutter.

---
+++ builder/liquibase/changelogs/changesets/pre-2.1/functions_cleanup.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+
+--### Functions cleanup. Deleting functions that are broken or redundant. ###---
+--changeset kpalis:drop_broken_functions context:general splitStatements:false
+DROP FUNCTION IF EXISTS addanalysistodataset(integer,integer);
+DROP FUNCTION IF EXISTS createdatasetmarker(integer, integer, real, real, real, jsonb);
+DROP FUNCTION IF EXISTS createmarkerlinkagegroup(integer, integer, integer, integer);
+
+DROP FUNCTION IF EXISTS getsampleqcmetadatabymarkerlistx(text, text);
+
+DROP FUNCTION IF EXISTS getcontactnamesbyrole(character varying);
+DROP FUNCTION IF EXISTS getcontactsbyrole(character varying);
+
+DROP FUNCTION IF EXISTS updatemarkerlinkagegroup(integer, integer, integer, integer, integer);
+
+--changeset kpalis:drop_useless_functions context:general splitStatements:false
+DROP FUNCTION IF EXISTS getallcontacts();
---
+++ builder/liquibase/changelogs/changesets/pre-2.1/rice_contacts_data.sql
@@ -0,0 +1,6 @@
+--liquibase formatted sql
+
+--changeset raza:rice_contact_uat context:seed_rice_uat splitStatements:false
+--this change, unfortunately, will (gently) break migration path for this particular context, but I got no choice as people don't want publicly accessible email addresses be deep in a database source code.
+--and I cannot just delete this file because then the migration path will not just gently break, it will go crashing down for existing instances.
+select * from createContact('Palis','Kevin','contact_7','kdp44@cornell.edu',( select array_agg(role_id) from role where role_name in ('PI','Curator','Admin')),1,current_date,NULL,NULL,(select organization_id from organization where name = 'GOBII Cornell Team'),'kpalis' );
