dbce88345c730841ef215f0e604611117c393d9f
Kevin Palis <kevin.palis@gmail.com>
2020-10-16 15:12:41 -0400

    Finally able to organize this mess better as I don't have to worry about existing instances - as will be merged safely on its own develop branch. This commit cleans up the changeset directory and puts files on their respective subdirectories.

--- builder/liquibase/changelogs/changesets/cv_germplasm_par_type_seed_data.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset raza:germplasm_par_type_cv context:seed_general splitStatements:false    
-
-select * from createCVinGroup('germplasm_prop',1,'par1_type','Parent 1 type of the germplasm name',10,null,null,1);
-select * from createCVinGroup('germplasm_prop',1,'par2_type','Parent 2 type of the germplasm name',11,null,null,1);
-select * from createCVinGroup('germplasm_prop',1,'par3_type','Parent 3 type of the germplasm name',12,null,null,1);
-select * from createCVinGroup('germplasm_prop',1,'par4_type','Parent 4 type of the germplasm name',13,null,null,1);
