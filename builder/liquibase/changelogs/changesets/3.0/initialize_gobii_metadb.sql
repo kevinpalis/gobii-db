6145a6df2e95563c9495f46b7a0be154f573bd16
Kevin Palis <kevin.palis@gmail.com>
2020-10-26 13:47:23 -0400

    Added comments. Modified the template table.

--- builder/liquibase/changelogs/changesets/3.0/initialize_gobii_metadb.sql
+++ builder/liquibase/changelogs/changesets/3.0/initialize_gobii_metadb.sql
@@ -13,18 +13,27 @@ CREATE TABLE crop (
 );
 
 COMMENT ON TABLE crop IS 'This table keeps track of all the crops in this GDM instance.'
+COMMENT ON COLUMN crop.name IS 'The name of the crop. Ex: maize'
+COMMENT ON COLUMN crop.database_name IS 'The postgres database name for this crop. The convention is gobii_<crop_name>.'
+COMMENT ON COLUMN crop.owner IS 'The postgres user that owns the crop database, typically, appuser.'
+COMMENT ON COLUMN crop.creation_date IS 'This is the date the crop was created - it gets auto-filled by the current date when the row was inserted.'
 
 CREATE TABLE template ( 
 	id					serial not null,
 	name				text not null,
-	crop_id				integer not null,
-	load_template		jsonb default '{}'::jsonb,
-	extract_template	jsonb default '{}'::jsonb,
+	crop_id				integer,
+	template			jsonb default '{}'::jsonb,
+	description			text,
 	creation_date		timestamp with time zone not null DEFAULT now(),
 	CONSTRAINT pk_template_id PRIMARY KEY ( id )
 );
 
-COMMENT ON TABLE template IS 'This table contains all templates for all the types of files we support.'
+COMMENT ON TABLE template IS 'This table contains the templates for all the types of files we support.'
+COMMENT ON COLUMN template.name IS 'The name of the template. This should be descriptive enough to identify the kind of data it applies to.'
+COMMENT ON COLUMN template.crop_id IS 'This identifies the crop this template applies to. Leave it empty if the template is generic and applies to all crops.'
+COMMENT ON COLUMN template.template IS 'This is the json template.'
+COMMENT ON COLUMN template.creation_date IS 'The date this template was created.'
+
 
 CREATE TABLE job ( 
 	id					serial not null,
@@ -38,6 +47,13 @@ CREATE TABLE job (
 );
 
 COMMENT ON TABLE job IS 'This table contains all jobs in the system along with their origin and status.'
+COMMENT ON COLUMN job.name IS 'The name of the job. This can reflect the name of the job from an external system (ex. rabbitMQ) if needed.'
+COMMENT ON COLUMN job.crop_id IS 'This identifies the crop this job is on.'
+COMMENT ON COLUMN job.type_id IS 'This identifies the type of job, ex: loading, extraction, etc.'
+COMMENT ON COLUMN job.status IS 'The current state of this job, ex: in_progress, success, fail.'
+COMMENT ON COLUMN job.message IS 'A freetext field the application can use to store any custom status message. Ex. Failed with error code 123, Data loaded successfully at 11-11-21 19:00:01.'
+COMMENT ON COLUMN job.creation_date IS 'The date this job was created.'
+
 
 CREATE TABLE job_type ( 
 	id					serial not null,
@@ -47,6 +63,8 @@ CREATE TABLE job_type (
 );
 
 COMMENT ON TABLE job_type IS 'This table contains all job types in the system, for example: data_loading, data_extraction.'
+COMMENT ON COLUMN job_type.name IS 'The name of the job type, ex: loading, extraction'
+
 
 CREATE TABLE status ( 
 	id					serial not null,
@@ -56,4 +74,4 @@ CREATE TABLE status (
 );
 
 COMMENT ON TABLE status IS 'This table contains all the possible status of a job, for example: in_progress, success, failed.'
-
+COMMENT ON COLUMN status.name IS 'The name of the status, ex: fail, success, in_progress, etc.'
