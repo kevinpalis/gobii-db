f8ad16d9feab52bcaa1a53eafa4d06c7922e4f71
Kevin Palis <kevin.palis@gmail.com>
2020-11-04 15:56:06 -0500

    Adding a more generic utility script to easily automate the addition of the meta db.

--- builder/rawbase/create_or_replace_db.sh
+++ builder/rawbase/create_or_replace_db.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #params: $1 = database_name; $2 = user_name; $3 = db container name; $4 = db container port; $5 = postgres pass
-#This is a utility script mainly used for automation in Bamboo. This creates a fresh database and initializes the crop schema on it.
-#Sample Bamboo call: sh create_or_replace_gobii_db.sh ${bamboo.crop.db.name} ${bamboo.db.username} ${bamboo.db.container.name} ${bamboo.db.port} ${bamboo.db.password}
+#This is a utility script mainly used for automation in Bamboo. This will create an empty database.
+
 echo "Params: " $1 $2 $3 $4 $5
 export PGPASSWORD=$5
 echo "Killing existing sessions for $1 if any..."
@@ -10,5 +10,3 @@ echo "Dropping database $1..."
 psql -h "$3" -p $4 -U $2 postgres -c "drop database $1;"
 echo "Creating database $1..."
 psql -h "$3" -p $4 -U $2 postgres -c "create database $1 owner $2;"
-echo "Populating database $1..."
-psql -h "$3" -p $4 -U $2 $1 -f build_gobii_pg.sql
