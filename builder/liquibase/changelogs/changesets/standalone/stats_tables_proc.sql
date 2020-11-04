6b74aa0e961e3f7f017cde1b2d8e59932183850b
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2020-11-04 10:53:50 +0800

    Added changesets definition, modified load_ifile_manager.py

--- builder/liquibase/changelogs/changesets/standalone/stats_tables_proc.sql
+++ builder/liquibase/changelogs/changesets/standalone/stats_tables_proc.sql
@@ -2,12 +2,11 @@
 
 --changeset rduldulao:stats_tables_procs splitStatements:false runOnChange:false
 
-
 CREATE OR REPLACE FUNCTION update_dataset_stats( dataset_id int, stat_name text, delta int ) RETURNS void
     LANGUAGE plpgsql AS $$
 
 BEGIN
-    EXECUTE format('UPDATE dataset_stats SET %I = %I + %s WHERE dataset_id = %I',
+    EXECUTE format('UPDATE dataset_stats SET %I = %I + %s WHERE dataset_id = %s',
         stat_name, stat_name, delta, dataset_id
     );
 
@@ -17,24 +16,24 @@ BEGIN
         );
     END IF;
 
-    EXECUTE format('UPDATE experiment_stats a, dataset b  SET a.%I = a.%I + %s WHERE b.dataset_id = %I AND b.experiment_id = a.experiment_id',
+    EXECUTE format('UPDATE experiment_stats SET %I = %I + %s FROM dataset WHERE dataset.dataset_id = %s AND dataset.experiment_id = experiment_stats.experiment_id',
         stat_name, stat_name, delta, dataset_id
     );
 
     IF NOT FOUND THEN
-        EXECUTE format('INSERT INTO experiment_stats(experiment_id, %I) SELECT a.experiment_id, %s FROM experiment a, dataset b WHERE b.dataset_id = %I AND b.experiment_id = a.experiment_id',
+        EXECUTE format('INSERT INTO experiment_stats(experiment_id, %I) SELECT a.experiment_id, %s FROM experiment a, dataset b WHERE b.dataset_id = %s AND b.experiment_id = a.experiment_id',
             stat_name, delta, dataset_id
         );
     END IF;
 
-    EXECUTE format('UPDATE project_stats a, experiment b, dataset c SET a.%I + %s WHERE c.dataset_id = %I AND c.experiment_id = b.experiment_id AND a.project_id = b.project_id',
+    EXECUTE format('UPDATE project_stats  SET %I = %I + %s FROM experiment, dataset WHERE dataset.dataset_id = %s AND dataset.experiment_id = experiment.experiment_id AND project_stats.project_id = experiment.project_id',
         stat_name, stat_name, delta, dataset_id
     );
 
     IF NOT FOUND THEN
-        EXECUTE format('INSERT INTO project_stats(project_id, %I) SELECT a.project_id, %s FROM project a, experiment b, dataset c WHERE c.dataset_id = %I AND c.experiment_id = b.experiment_id AND b.project_id = a.project_id',
+        EXECUTE format('INSERT INTO project_stats(project_id, %I) SELECT a.project_id, %s FROM project a, experiment b, dataset c WHERE c.dataset_id = %s AND c.experiment_id = b.experiment_id AND b.project_id = a.project_id',
             stat_name, delta, dataset_id
         );
-    END IF;
+	END IF;
 END;
 $$
