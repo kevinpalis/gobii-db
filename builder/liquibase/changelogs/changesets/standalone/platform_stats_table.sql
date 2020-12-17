c29628fe407a053c022a89726149e3b11a88c6d7
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2020-12-18 01:25:13 +0800

    Added platform_stats table def

---
+++ builder/liquibase/changelogs/changesets/standalone/platform_stats_table.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+
+--changeset rduldulao:init_plat_stats_tables splitStatements:false runOnChange:false
+-- initialize tables
+
+DROP TABLE IF EXISTS platform_stats;
+CREATE TABLE platform_stats(
+    platform_id INTEGER PRIMARY KEY REFERENCES platform(platform_id) ON DELETE CASCADE,
+    protocol_count BIGINT DEFAULT 0,
+    vendor_protocol_count BIGINT DEFAULT 0,
+    experiment_count BIGINT DEFAULT 0,
+    marker_count BIGINT DEFAULT 0,
+);
+
+
