9352b51dd7cfe5a943ad5205dcb1181590795c3a
vg249 <vg249@cornell.edu>
2021-01-20 08:59:30 -0500

    master changelog is updated with 2.2.3 change logs
    checked out only database marker and dnarun count stats for parent change

---
+++ builder/liquibase/changelogs/changesets/standalone/stats_tables.sql
@@ -0,0 +1,35 @@
+--liquibase formatted sql
+
+--changeset rduldulao:create_stats_tables splitStatements:false runOnChange:false
+
+-- create tables for counting
+
+DROP TABLE IF EXISTS dataset_stats;
+
+CREATE TABLE  dataset_stats(
+    dataset_id INTEGER PRIMARY KEY REFERENCES dataset(dataset_id) ON DELETE CASCADE,
+    marker_count BIGINT DEFAULT 0,
+    dnarun_count BIGINT DEFAULT 0
+);
+
+
+DROP TABLE IF EXISTS experiment_stats;
+CREATE TABLE IF NOT EXISTS experiment_stats(
+    experiment_id INTEGER PRIMARY KEY REFERENCES experiment(experiment_id) ON DELETE CASCADE,
+    dataset_count BIGINT DEFAULT 0,
+    marker_count BIGINT DEFAULT 0,
+    dnarun_count BIGINT DEFAULT 0
+);
+
+
+DROP TABLE IF EXISTS project_stats;
+CREATE TABLE project_stats(
+    project_id INTEGER PRIMARY KEY REFERENCES project(project_id) ON DELETE CASCADE,
+    experiment_count BIGINT DEFAULT 0,
+    dataset_count BIGINT DEFAULT 0,
+    marker_count BIGINT DEFAULT 0,
+    dnarun_count BIGINT DEFAULT 0,
+    dnasample_count BIGINT DEFAULT 0
+);
+
+
